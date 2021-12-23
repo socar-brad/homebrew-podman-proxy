6a75a5a3e04ac4dffe2b8f647de96ea3b0abb87d
Brad Kim <brad@socar.kr>
2021-12-23 15:33:59 +0900

    podman-proxy v0.0.2

--- Formula/podman-proxy.rb
+++ Formula/podman-proxy.rb
@@ -3,12 +3,11 @@ class PodmanProxy < Formula
   homepage ""
   license ""
   url "https://github.com/socar-brad/podman-proxy.git",
-      tag:      "v0.0.1",
-      revision: "3ce7abbb8eadb99b7690740c466f1d0830b06f15"
+      tag:      "v0.0.2",
+      revision: "b5f22c30bce4668c0ad63f5b4259ea238d89d350"
   head "https://github.com/socar-brad/podman-proxy.git", branch: "main"
 
-  # depends_on "cmake" => :build
-
+  depends_on "podman"
   depends_on "go" => :build
 
   def install
@@ -21,24 +20,9 @@ class PodmanProxy < Formula
       system "go", "build", "-o", "#{bin}/podman-proxy"
     end
 
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test podman-proxy`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    # system "false"
   end
 end
 
